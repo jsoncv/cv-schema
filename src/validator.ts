49f8686cf3c90f07ef488749b1aa7f18b3b28b98
Amin Zabardsat <zabardast.amin@metu.edu.tr>
2021-08-19 23:11:26 +0300

    Replacing the validator from Core

--- src/validator.ts
+++ src/validator.ts
@@ -1,9 +1,4 @@
-import Ajv from 'ajv'
-import schema from './schema.json'
+import { validator } from '@jsoncv/core'
 import sample from './sample.json'
 
-const ajv = new Ajv()
-const validate = ajv.compile(schema)
-const valid = validate(sample)
-
-if (!valid) console.log(validate.errors)
+validator(sample)
